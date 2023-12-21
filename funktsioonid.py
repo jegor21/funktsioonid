1a28dce191053d1e332f5b9e8a3cf3ce7e00c7d0
opilane <opilane@HP-CZC2349HV2>
2023-12-21 16:22:26 +0200

    Add project files.

---
+++ funktsioonid.py
@@ -0,0 +1,52 @@
+from module1 import *
+
+#S=summa3(3.5,2,4.9)
+#print(S)
+#a=float(input("Arv1: "))
+#b=float(input("Arv2: "))
+#c=float(input("Arv3: "))
+#s3=summa3=(a,b,c)
+
+
+
+#Ülesanne 1
+#v=arithmetic(float(input("Arv1: ")),float(input("Arv2: ")), input("Tehing: "))
+#print("Vastus = ",v)
+
+#Ülesanne 2
+#v=is_year_leap(int(input("Sisestage aasta: ")))
+#print(v)
+
+#Ülesanne 3
+#S,p,d=square(4)
+#print("Pindala",S)
+#print("Ümbermõõt",p)
+#print("Läbimõõt",d)
+
+#Ülesanne 4
+#v=season(int(input("Kuu: ")))
+#print(v)
+
+
+#Ülesanne 5
+
+
+#Ülesanne 6
+
+#s=is_prime()
+#print(s)
+
+
+
+
+#Ülesanne 7
+
+
+
+
+
+#Ülesanne 8
+
+
+
+
---
+++ module1.py
@@ -0,0 +1,136 @@
+from random import randint
+def summa3(arv1: float, arv2: float, arv3:float)->float:
+    """Tagastab kolme arvu summa
+    
+    :param float arv1: Esimene arv
+    :param float arv2: Teine arv
+    :param float arv3: Kolmas arv
+    :rtype: float
+
+
+
+    """
+    summa=arv1+arv2+arv3
+    return summa
+
+#Ülesanne 1
+
+def arithmetic(a1: float, a2: float, operation: str)->any:
+    
+    """Lihtne kalkulaator
+    +
+    -
+    *
+    /
+    :param float a1: Esimene arv
+    :param float a2: Teine arv
+    :param str operation: aritmeetiline tehing
+    :rtype: var Määramata tüüp(float or str)
+
+    """
+
+    if operation in ["+","-","*","/"]:
+        if a2==0 and operation=="/":
+            vastus="DIV/0"
+        else:
+            vastus=eval(str(a1)+operation+str(a2))
+    else:
+        vastus="Tundmatu operatsioon"
+    return vastus
+
+
+#Ülesanne 2
+
+def is_year_leap (year: int)->bool:
+    """Liigaasta leidmine
+    Tagastab True, kui liigaasta ja False kui on tavaline aasta.
+    :param int aasta: aasta number
+    :rtype: bool tagastab loogilises formaadis tulemus
+
+    """
+    if year%4==0:
+        v=True
+    else:
+        v=False
+    return v
+
+
+#Ülesanne 3
+
+def square(a: float):
+    """
+
+    """
+    S=a**2
+    p=4*a
+    d=(2)**(1/2)*a
+    return "Pindala"+str(S),"Ümbermõõt"+str(p),"Läbimõõt"+str(d)
+
+#Ülesanne 4
+
+def season(aeg: int)->str:
+    """
+
+    """
+    if aeg in [1,2,12]:
+        v="Talv"
+    elif aeg in [3,4,5]:
+        v="Kevad"
+    elif aeg in [6,7,8]:
+        v="Suvi"
+    elif aeg in [9,10,11]:
+        v="Sügis"
+    else:
+        v="Vale andmed"
+    return v
+
+
+#Ülesanne 5
+
+def bank(a: float, years: int)->float:
+    """
+
+    """
+    for i in range(years):
+        a*=1.1
+    return a 
+
+
+
+#Ülesanne 6
+def is_prime(a=randint(0,1000))->bool:
+    """
+
+    """
+    print(a)
+    v=False
+    for i in range(2,a):
+        if a%i==0:
+            y=True
+
+    return v
+
+#Ülesanne 7
+
+def date(päev: int, kuu: int, aasta: int):
+    """
+
+    """
+
+    if päev in range(1, 31) and kuu in [1,3,5,7,8,10,12]:
+        v=True
+    elif päev in range(1, 29) and kuu == 2 and is_year_leap:
+        v=True
+    elif päev in range(1, 30) and kuu in [2,4,6,9,11]:
+        v=True
+    else:
+        v=False
+    return v
+
+
+
+
+
+
+
+
